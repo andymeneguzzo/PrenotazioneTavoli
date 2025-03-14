d241feab14371ef392ff5a58edbed0fdcb146750
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-03-14 15:44:17 +0100

    Initial commit

---
+++ src/test/java/com/andy/prenotazionetavoli/PrenotazioneTavoliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.prenotazionetavoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrenotazioneTavoliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
